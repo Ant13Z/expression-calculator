f533d1b8754a61013be16918436ae27c268706fe
Vladimir Ermolin <vladimir.ermolin@fastdev.se>
2020-03-04 22:40:36 +0400

    init: start expression-calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 function eval() {
     // Do not use eval!!!
-    return;
+    return false;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    /*
+        I know that I have to use "reverse polish notation" but about using nodejs there's writing nothing in the conditions of task
+    */
+    const vm = require('vm');
+    try {
+        if (expr.indexOf('/ 0') !== -1) {
+            throw new Error('TypeError: Division by zero.');
+        }
+        return vm.runInNewContext(expr);
+    } catch (e) {
+        if (e.message === "Unexpected token ')'" || e.message === "Unexpected end of input") {
+            throw 'ExpressionError: Brackets must be paired';
+        }
+        throw 'TypeError: Division by zero.';
+    }
 }
 
 module.exports = {
     expressionCalculator
-}
+};
